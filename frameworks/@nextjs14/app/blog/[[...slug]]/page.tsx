b878cf4310b3c6de244eec89722f26bfbeeb0768
Mohaa <49195865+cra2ycoder@users.noreply.github.com>
2024-04-07 23:32:34 +0530

    nextjs14 routing samples added

---
+++ frameworks/@nextjs14/app/blog/[[...slug]]/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Slug() {
+  return (
+    <>
+      <div>Slug</div>
+      <p>
+        when we put double square brackets over the folder name, it will
+        consider the parent folder page.tsx (ex: blog) otherwise it will lead to
+        404 page
+      </p>
+    </>
+  )
+}
+
+export default Slug
