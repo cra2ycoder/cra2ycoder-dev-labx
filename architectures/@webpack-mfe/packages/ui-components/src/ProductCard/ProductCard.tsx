953bd4d0ee629148d968d99b5e9bdc1a837a5e1f
Mohaa <49195865+cra2ycoder@users.noreply.github.com>
2025-01-18 23:01:14 +0530

    Product card template added - initial code

--- architectures/@webpack-mfe/packages/ui-components/src/ProductCard/ProductCard.tsx
+++ architectures/@webpack-mfe/packages/ui-components/src/ProductCard/ProductCard.tsx
@@ -1,5 +1,20 @@
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+
 function ProductCard() {
-  return <div>ProductCard</div>
+  return (
+    <Box>
+      <img src="" alt="" />
+      <Typography>Product Title</Typography>
+      <Typography>Product Desc</Typography>
+      <Typography>Product category</Typography>
+      <Typography>Product Price</Typography>
+      <Button variant="contained" color="primary">
+        Add to Cart
+      </Button>
+    </Box>
+  )
 }
 
 export { ProductCard }
