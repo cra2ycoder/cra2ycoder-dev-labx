946e91b78be66faf8b77b35d6f273f0c3a2aa02f
Mohaa <49195865+cra2ycoder@users.noreply.github.com>
2025-01-21 19:17:13 +0530

    search box added - with no stylings

--- architectures/@webpack-mfe/packages/ui-components/src/Header/Header.tsx
+++ architectures/@webpack-mfe/packages/ui-components/src/Header/Header.tsx
@@ -1,7 +1,10 @@
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Stack from '@mui/material/Stack'
+import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
+import TextField from '@mui/material/TextField'
+
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined'
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined'
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined'
@@ -13,6 +16,15 @@ function Header() {
         <Typography variant="h6" component="div">
           E-Commerce Store
         </Typography>
+        <Box sx={{ width: '50%' }}>
+          <TextField
+            id="standard-basic"
+            label="Search for products, category and more"
+            variant="filled"
+            size="small"
+            sx={{ width: '100%' }}
+          />
+        </Box>
         <Stack direction="row" spacing={4}>
           <FavoriteBorderOutlinedIcon />
           <ShoppingCartOutlinedIcon />
