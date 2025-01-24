f650fb7ad94c56776d6aa8d747d7db84bfe8c6eb
Mohaa <49195865+cra2ycoder@users.noreply.github.com>
2025-01-24 22:23:59 +0530

    more examples

--- core/@javascript/src/regex/index.js
+++ core/@javascript/src/regex/index.js
@@ -13,6 +13,10 @@ const stringToBeMatched =
 const exactMatch = /test/
 console.log(exactMatch.test(stringToBeMatched)) // true
 
+// any one char match or exact match
+const anyOneCharFromGiven = /[abcd]/
+console.log(anyOneCharFromGiven.test(stringToBeMatched)) // true
+
 // any one character matching between a to z (note: only lower case characters)
 const anyOneCharMatch = /[a-z]/
 console.log(anyOneCharMatch.test(stringToBeMatched)) // true
@@ -25,6 +29,15 @@ console.log(anyOneCharCaseInsensitiveMatch.test(stringToBeMatched)) // true
 const anyOneCharCaseInsensitiveMatch1 = /[a-z]/i
 console.log(anyOneCharCaseInsensitiveMatch1.test(stringToBeMatched)) // true
 
-// only words matching (without spacing)
+// only char(s) matching incl. numbers but with no spaces and no special characters
 const wordMatch = /\w+/g
 console.log(wordMatch.test(stringToBeMatched)) // true
+
+// a char to be twice time repeated (must) 'aa'
+const charLengthLimit = /a{2}/
+
+// minimum one `a` and can be repeated 'n' of times
+const charNoLengthLimit = /a{1,}/
+
+console.log(charLengthLimit.test(stringToBeMatched)) // false
+console.log(charNoLengthLimit.test(stringToBeMatched)) // true
