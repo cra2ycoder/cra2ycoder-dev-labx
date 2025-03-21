70beeed381d36a4a95828e5a2ee5b76855def169
Mohaa <49195865+cra2ycoder@users.noreply.github.com>
2025-03-21 07:38:20 +0530

    html examples

--- core/@html/index.ts
+++ core/@html/index.ts
@@ -1,12 +1,13 @@
 import * as express from 'express'
 
 const server = express()
+const PORT = 3000
 
 server.use(express.static('./exercises'))
 server.get('/*', (req, res) => {
   res.send('hello, world')
 })
 
-server.listen(3000, () => {
-  console.log('server listening >>>>')
+server.listen(PORT, () => {
+  console.log(`server listening at http://localhost:${PORT}`)
 })
