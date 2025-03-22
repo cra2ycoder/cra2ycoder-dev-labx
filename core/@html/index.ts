e797ba84a0f87f747de491f1b257c341eccc72d0
Mohaa <49195865+cra2ycoder@users.noreply.github.com>
2025-03-22 21:34:24 +0530

    sample form created

--- core/@html/index.ts
+++ core/@html/index.ts
@@ -3,7 +3,15 @@ import * as express from 'express'
 const server = express()
 const PORT = 3000
 
+server.use(express.json())
+
 server.use(express.static('./exercises'))
+server.post('/api/comments', async (req, res) => {
+  res.status(200).send({
+    data: req.body,
+  })
+})
+
 server.get('/*', (req, res) => {
   res.send('hello, world')
 })
