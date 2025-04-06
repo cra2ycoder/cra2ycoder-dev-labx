bda942484423f68852c23ea1f37b89c13bba39ca
Mohaa <49195865+cra2ycoder@users.noreply.github.com>
2025-04-06 21:08:44 +0530

    click handler added for todo list component

--- states/redux/src/components/TodoList.tsx
+++ states/redux/src/components/TodoList.tsx
@@ -1,17 +1,33 @@
+import { useRef } from 'react'
+
 type TTodoItem = {
   label?: string
 }
 
 function TodoItem(props: Readonly<TTodoItem>) {
+  const editTask = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(`edit task`)
+  }
+
+  const deleteTask = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(`delete task`)
+  }
+
   return (
     <>
       <li className="flex items-center justify-between w-full">
         <p className="">{props.label}</p>
         <div className="flex gap-2">
-          <button className="border rounded-md bg-black text-white py-2 px-4 text-sm">
+          <button
+            className="border rounded-md bg-black text-white py-2 px-4 text-sm"
+            onClick={editTask}
+          >
             Edit
           </button>
-          <button className="border rounded-md bg-black text-white py-2 px-4 text-sm">
+          <button
+            className="border rounded-md bg-black text-white py-2 px-4 text-sm"
+            onClick={deleteTask}
+          >
             Delete
           </button>
         </div>
@@ -22,20 +38,40 @@ function TodoItem(props: Readonly<TTodoItem>) {
 }
 
 function TodoList() {
+  const formRef = useRef<HTMLFormElement>(null)
   const list: string[] = []
+
+  const createTask = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    const formData = new FormData(formRef.current as HTMLFormElement)
+    const userInput = formData.get('task')
+
+    if (userInput) {
+      //@todo post the data to save into state
+    }
+  }
+
   return (
     <div className="flex w-full flex-wrap">
-      <form action="#" className="w-full flex flex-wrap gap-2 items-center">
-        <label htmlFor="task" id="task" className="">
+      <form
+        action="#"
+        className="w-full flex flex-wrap gap-2 items-center"
+        id="task-form"
+        ref={formRef}
+      >
+        <label htmlFor="task" id="task">
           Enter your task:
         </label>
         <input
           type="text"
           name="task"
           id="task"
-          className="border rounded-md w-dvw h-[40px] border-gray-300"
+          className="border rounded-md w-dvw h-[40px] border-gray-300 px-4"
         />
-        <button className="border rounded-md bg-black text-white py-2 px-4 text-sm">
+        <button
+          className="border rounded-md bg-black text-white py-2 px-4 text-sm cursor-pointer"
+          onClick={createTask}
+        >
           Create a Task
         </button>
       </form>
