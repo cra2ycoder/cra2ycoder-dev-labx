6c7cd36300b6198535b19ca836ceaadb641b7434
Mohaa <49195865+cra2ycoder@users.noreply.github.com>
2025-05-22 14:10:42 +0530

    error resolving

--- ui/reactjs19/app/page.tsx
+++ ui/reactjs19/app/page.tsx
@@ -2,7 +2,7 @@ import { Suspense } from 'react'
 import Counter from '@/components/Counter'
 import Accordion from '@/components/Accordion'
 import ContactForm from '@/components/ContactForm'
-import ApiComponent from '@/components/ApiComponent'
+// import ApiComponent from '@/components/ApiComponent'
 import SimpleForm from '@/components/SimpleForm'
 import CommentForm from '@/components/CommentForm'
 import PriceManager from '@/components/PriceManager'
@@ -22,9 +22,9 @@ export default function Home() {
       <hr />
       <SimpleForm />
       <hr />
-      <Suspense fallback={<div>loading...</div>}>
+      {/* <Suspense fallback={<div>loading...</div>}>
         <ApiComponent />
-      </Suspense>
+      </Suspense> */}
       <hr />
       <hr />
       <CommentForm />
