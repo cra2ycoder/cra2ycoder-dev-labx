0f6744e820010028157c52e9fe31957c9a0ba3fa
Mohaa <49195865+cra2ycoder@users.noreply.github.com>
2025-07-22 14:46:51 +0530

    Add MongoDB query examples for sorting and projection
    Added various MongoDB query examples demonstrating sorting (ascending, descending, multiple fields), skipping and limiting results, and shaping results using projection including array slicing and $elemMatch.

--- core/mongodb/__playground__/movies.mongodb.js
+++ core/mongodb/__playground__/movies.mongodb.js
@@ -17,3 +17,31 @@ db.movies.find({ genre: { $size: 2 } }).pretty()
 db.movies.find({ 'meta.aired': 2018 })
 
 db.movies.find({ 'meta.rating': { $gt: 8, $lt: 10 } }).pretty()
+
+//ascending sort
+db.movies.find().sort({ 'meta.rating': 1 })
+
+//descending sort
+db.movies.find().sort({ 'meta.rating': -1 })
+
+// multiple sort
+db.movies.find().sort({ 'meta.rating': -1, 'meta.visitors': 1 })
+
+// skip
+db.movies.find().sort({ 'meta.rating': -1 }).skip(10).limit(100)
+
+// shaping the results with projection
+// 1 - required
+// 0 - not required
+db.movies.find({}, { 'meta.rating': 1, 'meta.visitors': 1, _id: 0 })
+
+// using projection in arrays
+db.movies
+  .find({ genres: 'Drama' }, { genres: { $elemMatch: { $eq: 'horror' } } })
+  .pretty()
+
+// slice with array
+db.movies.find({}, { genres: { $slice: 2 } }).pretty()
+
+// skipping 1st element and taking 2 results except 1st element
+db.movies.find({}, { genres: { $slice: [1, 2] } }).pretty()
